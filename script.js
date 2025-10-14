dbee3e5f512508244abb9c209f546cc0aaf4ce96
Pratheek M <dflpratheekm@gmail.com>
2025-10-14 16:09:16 +0530

    Add button click event to update output text

---
+++ script.js
@@ -0,0 +1,6 @@
+const button= document.getElementById("clickMe");
+const output=document.getElementById("output");
+button.addEventListener("click",() => {
+  output.textContent = "You clicked the button. Why. ";
+}
+                        )
